9fd95fc228ff4fb2bfc182b71dec1eac4bbe7ab5
JGcampos <joaogabriel.oi@hotmail.com>
2025-07-16 19:23:03 -0300

    feat: Restore authentication logic and routing in HomePage component

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { ROUTES } from "@/core/routes/route-constants";
+import { useAuthStore } from "@/feature/authentication/store/use-auth.store";
+import { redirect } from "next/navigation";
+import { use } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function HomePage({
@@ -7,14 +11,14 @@ export default function HomePage({
 }: {
   params: Promise<{ lang: string }>;
 }) {
-  // const { lang } = use(params);
+  const { lang } = use(params);
 
-  // const { isAuthenticated } = useAuthStore();
+  const { isAuthenticated } = useAuthStore();
   const { ready } = useTranslation();
 
   if (!ready) {
     return <>carregando.</>;
   }
 
-  // redirect(`/${lang}${isAuthenticated ? ROUTES.DASHBOARD : ROUTES.LOGIN}`);
+  redirect(`/${lang}${isAuthenticated ? ROUTES.DASHBOARD : ROUTES.LOGIN}`);
 }
